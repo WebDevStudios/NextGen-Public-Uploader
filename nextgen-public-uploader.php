c46a989aac492583f5a9b5440dc70e4be08a8714
Michael Beckwith <tw2113@gmail.com>
2015-12-08 23:37:33 -0600

    mmm brackets

--- nextgen-public-uploader.php
+++ nextgen-public-uploader.php
@@ -27,8 +27,9 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-if ( ! defined( 'ABSPATH' ) )
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
+}
 
 class NGGallery_Public_uploader {
 
