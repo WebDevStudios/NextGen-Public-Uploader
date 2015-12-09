da67a557295f5c68cb0733b70c5ea3af44237600
Michael Beckwith <tw2113@gmail.com>
2015-12-08 23:42:08 -0600

    nitpicky spacing cleanup from generous PR

--- inc/class.npu_uploader.php
+++ inc/class.npu_uploader.php
@@ -24,58 +24,55 @@ class UploaderNggAdmin extends nggAdmin {
 	public $strGalleryPath   = '';
 	public $blnRedirectPage  = false;
 
-	function upload_images()
-	{
+	function upload_images() {
 		$storage        = C_Gallery_Storage::get_instance();
 		$image_mapper   = C_Image_Mapper::get_instance();
 		$gallery_mapper = C_Gallery_Mapper::get_instance();
 
 		// Get Gallery ID
-		$galleryID = (int)$_POST['galleryselect'];
-		if ( $galleryID == 0 ) {
-			if( get_option( 'npu_default_gallery' ) ) {
-				$galleryID = get_option( 'npu_default_gallery' );
-			} else {
+		$galleryID = absint( $_POST['galleryselect'] );
+		if ( 0 == $galleryID ) {
+			$galleryID = get_option( 'npu_default_gallery' );
+			if ( empty( $galleryID ) ) {
 				self::show_error( __( 'No gallery selected.', 'nextgen-public-uploader' ) );
 				return;
 			}
 		}
 
 		// Get the Gallery
-		$gallery = $gallery_mapper->find($galleryID);
-		if (!$gallery->path)
-		{
-			self::show_error(__('Failure in database, no gallery path set.', 'nextgen-public-uploader'));
+		$gallery = $gallery_mapper->find( $galleryID );
+		if ( ! $gallery->path ) {
+			self::show_error( __('Failure in database, no gallery path set.', 'nextgen-public-uploader' ) );
 			return;
 		}
 
 		// Read Image List
-		foreach($_FILES as $key => $value) {
-			if ($_FILES[$key]['error'] == 0)
-			{
+		foreach( $_FILES as $key => $value ) {
+			if ( 0 == $_FILES[ $key ]['error'] ) {
 				try {
-					if ($storage->is_image_file($_FILES[$key]['tmp_name']))
-					{
+					if ( $storage->is_image_file( $_FILES[ $key ]['tmp_name'] ) ) {
 						$image = $storage->object->upload_base64_image(
 							$gallery,
-							file_get_contents($_FILES[$key]['tmp_name']),
+							file_get_contents( $_FILES[ $key ]['tmp_name'] ),
 							$_FILES[$key]['name']
 						);
 
-						if (get_option('npu_exclude_select'))
-						{
+						if ( get_option( 'npu_exclude_select' ) ) {
 							$image->exclude = 1;
-							$image_mapper->save($image);
+							$image_mapper->save( $image );
 						}
 
 						// Add to Image and Dir List
 						$this->arrImgNames[]  = $image->filename;
 						$this->arrImageIds[]  = $image->id();
 						$this->strGalleryPath = $gallery->path;
-					}
-					else {
-						unlink($_FILES[$key]['tmp_name']);
-						self::show_error('<strong>' . $_FILES[$key]['name'] . '</strong>' . __( 'is not a valid file.', 'nextgen-public-uploader'));
+					} else {
+						unlink( $_FILES[ $key ]['tmp_name'] );
+						$error_msg = sprintf(
+							__( '<strong>%s</strong> is not a valid file.', 'nextgen-public-uploader' ),
+							$_FILES[ $key ]['name']
+						);
+						self::show_error( $error_msg );
 						continue;
 					}
 				}
